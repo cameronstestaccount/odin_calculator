a1af19bb38b0d0c66d1b8537e5de6fffef570f96
Cameron Testing <camriesen2@gmail.com>
2021-11-16 14:58:14 -0500

    Implement operation functions and operation caller

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let test = document.createElement('h2');
-console.log("test");
-test.innerText = "test";
-document.body.appendChild(test);
+function add (a, b){
+    return a + b;
+}
+function sub (a, b){
+    return a - b;
+}
+function mul (a, b){
+    return a * b;
+}
+function div (a, b){
+    return a / b;
+}
+
+function operator(op, a, b){
+    switch(op){
+        case '+': return add(a, b);
+        case '-': return sub(a, b);
+        case '*': return mul(a, b);
+        case '/': return div(a, b);
+        default: 
+            console.log("[ERROR] Unknown operator");
+            return;
+    }
+}
